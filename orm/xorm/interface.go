72dde4b70682d6ca9067007a828211d5dd947c28
evalphobia <evalphobia@gmail.com>
2015-07-31 16:02:04 +0900

    [xorm] Added LastSQL()

--- orm/xorm/interface.go
+++ orm/xorm/interface.go
@@ -101,7 +101,8 @@ type Session interface {
 	InsertOne(interface{}) (int64, error)
 	Update(interface{}, ...interface{}) (int64, error)
 	Delete(interface{}) (int64, error)
-
+	
+	LastSQL() (string, []interface{})
 	Sync2(...interface{}) error
 }
 
